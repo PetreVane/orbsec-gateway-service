758a92ba2386ff4c499fb655dcb65a1993a308ba
Petre Vane <petre.vane@gmail.com>
2022-02-22 09:01:25 +0100

    Changes: - added handler for connection failed exceptions

--- src/main/java/com/orbsec/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/orbsec/gatewayservice/GatewayServiceApplication.java
@@ -1,8 +1,11 @@
 package com.orbsec.gatewayservice;
 
+import com.orbsec.gatewayservice.exceptions.GlobalExceptionConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +13,11 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	GlobalExceptionConfiguration globalExceptionConfiguration() {
+		return new GlobalExceptionConfiguration();
 	}
 
 }
---
+++ src/main/java/com/orbsec/gatewayservice/exceptions/GlobalExceptionConfiguration.java
@@ -0,0 +1,46 @@
+package com.orbsec.gatewayservice.exceptions;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Slf4j
+@RequiredArgsConstructor
+public class GlobalExceptionConfiguration implements ErrorWebExceptionHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable exception) {
+        ServerHttpResponse response = exchange.getResponse();
+
+        if (response.isCommitted()) {
+            return Mono.error(exception);
+        }
+
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        if (exception instanceof ResponseStatusException) {
+            response.setStatusCode(((ResponseStatusException) exception).getStatus());
+        }
+
+        return response
+                .writeWith(Mono.fromSupplier(() -> {
+                    DataBufferFactory bufferFactory = response.bufferFactory();
+                    try {
+                        return bufferFactory.wrap(objectMapper.writeValueAsBytes(exception.getMessage()));
+                    } catch (JsonProcessingException e) {
+                        log.warn("Error writing response", exception);
+                        return bufferFactory.wrap(new byte[0]);
+                    }
+                }));
+    }
+}
